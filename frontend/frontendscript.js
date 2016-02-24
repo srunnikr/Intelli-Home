1422c58a9b9cc83df196ae055ae3d33e30349a38
srunnikr <srunnikr@eng.ucsd.edu>
2016-02-24 11:31:52 -0800

    Added basic graphing facility for temperature

--- frontend/frontendscript.js
+++ frontend/frontendscript.js
@@ -1,3 +1,7 @@
+var d = new Date();
+var readings = [12.3, 14.5, 22.3, 11.4, 14.6]
+var labels = ['12PM', '1PM', '2PM', '3PM', '4PM']
+
 $(document).ready( function () {
 	// Set the status message to test javascript functionality
 	console.log("javascript on");
@@ -7,6 +11,7 @@ $(document).ready( function () {
 	updateHumidity(90);
 	updateLampState("ON");
 	updateIntrusionState("NOT GOOD");
+	drawGraph(readings, labels);
 
 	// Establish connection via socket io
 	var socket = io.connect('127.0.0.1');
@@ -33,10 +38,38 @@ $(document).ready( function () {
 
 });
 
-
+function drawGraph(data, labels) {
+	var buyerData = {
+			labels : labels,
+			datasets : [
+				{
+					fillColor : "rgba(172,194,132,0.4)",
+					strokeColor : "#ACC26D",
+					pointColor : "#fff",
+					pointStrokeColor : "#9DB86D",
+					data : data
+				}
+			]
+		}
+	var buyers = document.getElementById('myChart').getContext('2d');
+    new Chart(buyers).Line(buyerData);
+}
 
 function updateTemperature(newValue) {
 	document.getElementById("temperatureValue").innerHTML = newValue;
+
+	// Update the temperature data base for the chart
+	readings.push(newValue);
+	var current_hour = d.getHours();
+	var label = "";
+	label += current_hour.toString();
+	if(current_hour < 12) {
+		label+="AM";
+	} else {
+		label += "PM";
+	}
+	labels.push(label);
+	drawGraph(readings, labels);
 }
 
 function updateHumidity(newValue) {
